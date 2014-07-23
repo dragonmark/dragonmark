c71d1e6f74876621db40a64f64ecbae5cafe0582
David Pollak <feeder.of.the.bears@gmail.com>
2014-07-23 15:10:01 -0700

    Got tests running

--- project.clj
+++ project.clj
@@ -3,49 +3,49 @@
   :url "http://dragonmark.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.cognitect/transit-clj "0.8.229"]
+                 [com.cognitect/transit-cljs "0.8.137"]                 
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
 
+  :plugins [[codox "0.8.10"]
+            [lein-cljsbuild "1.0.3"]
+            [com.keminglabs/cljx "0.4.0"]
+            [com.cemerick/clojurescript.test "0.3.1"]]
+
   :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  
-  :source-paths ["src/cljx"]
-  :test-paths ["target/test-classes"]
+
+  :codox {:defaults {:doc/format :markdown}
+          :sources ["target/generated/src"]
+          :output-dir "doc/codox"
+          :src-linenum-anchor-prefix "L"
+          :src-uri-mapping {#"target/generated/src" #(str "src/" % "x")}}
+
+  :cljx
+  {:builds
+   [{:source-paths ["src"], :output-path "target/generated/src", :rules :clj}
+    {:source-paths ["test"], :output-path "target/generated/test", :rules :clj}
+    {:source-paths ["src"], :output-path "target/generated/src", :rules :cljs}
+    {:source-paths ["test"], :output-path "target/generated/test", :rules :cljs}]}
+
+  :source-paths ["src" "target/generated/src"]
+
+  :test-paths   ["test" "target/generated/test"]
 
   :hooks [cljx.hooks]
 
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :clj}
+  :cljsbuild
+  {:builds
+   [{:source-paths ["target/generated/src" "target/generated/test"]
+     :compiler {:output-to "target/main.js"}}]
+   :test-commands {"unit-tests" ["phantomjs" :runner "target/main.js"]}}
 
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :cljs}
+  :aliases
+  {"test-cljs" ["do" ["cljx" "once"] ["cljsbuild" "test"]]
+   "test-all"  ["do" ["test"] ["cljsbuild" "test"]]}
 
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test-classes"
-                   :rules :cljs}]}
-
-  :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]}
-              :builds [{:source-paths ["target/classes" "target/test-classes"]
-                        :compiler {:output-to "target/testable.js"
-                                   :optimizations :advanced
-                                   :pretty-print true}}]}
-
-  :profiles {:dev {:plugins [[org.clojure/clojurescript "0.0-2202"]
-                             [com.cemerick/clojurescript.test "0.3.0"]
-                             [com.keminglabs/cljx "0.3.2"]
-                             [lein-cljsbuild "1.0.3"]]
-                   :aliases {"cleantest" ["do" "clean," "cljx" "once," "test,"
-                                          "cljsbuild" "test"]
-                             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}}}
-
-  :repositories {"sonatype-oss-public"
-                 "https://oss.sonatype.org/content/repositories/snapshots/"}
-
+  :profiles
+  {:provided {:dependencies [[org.clojure/clojurescript "0.0-2268"]]}}
+  
 )
